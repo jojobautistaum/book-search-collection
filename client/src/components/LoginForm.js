34d6e62c9b381f5a8a5908e90aed340f1e13349b
Jojo Bautista <gjojob@yahoo.com>
2022-03-14 12:05:30 -0500

    convert sign up and login to graphql

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -28,15 +28,15 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await login(userFormData);
+      const { data } = await login({ variables: { ...userFormData } });
 
-      if (!response.ok) {
-        throw new Error("something went wrong!");
-      }
+      // if (!response.ok) {
+      //   throw new Error("something went wrong!");
+      // }
 
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // const { token, user } = await response.json();
+      // console.log(user);
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
